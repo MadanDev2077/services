281822df8e06530e69e5d2b846ea78d83e1dbe82
NetbixTechnology <134360436+NetbixTechnology@users.noreply.github.com>
2025-05-22 13:34:31 +0530

    desc text modify

--- src/components/CommonLayouts/SignUpCard.js
+++ src/components/CommonLayouts/SignUpCard.js
@@ -17,12 +17,12 @@ const SignUpCard = () => {
           <div className=" border border-[#ffffff80] rounded-4xl px-2 relative py-4 md:py-5 bg-[#ffffff2b]">
             <input
               type="text"
-              className=" outline-0 placeholder:text-[#ffffff80] text-white px-2 text-[15px] "
+              className=" outline-0 placeholder:text-[#ffffff80] text-white px-2 text-[15px] w-full "
               placeholder="Enter Your Email"
             />
-            <div className=" absolute top-1/2 right-2 -translate-y-1/2 h-[85%]">
+            <div className=" absolute top-1/2 right-2 -translate-y-1/2 h-[85%]  ">
               <RoundedButton
-                classes="!bg-[#d1fe67] !text-black font-semibold h-full flex items-center justicy-center"
+                classes="!bg-[#d1fe67] !text-black hover:!text-white !px-7 font-semibold h-full button-hover-black flex items-center justicy-center"
                 text={"Get Started"}
               />
             </div>
